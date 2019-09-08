cddacff62b1076a356ec134d5ccce05fa6e0ae29
lucasbbudelon <lucasbbudelon@gmail.com>
2019-09-08 18:26:03 -0300

    icon times red

--- development-environment/check-development-environment.sh
+++ development-environment/check-development-environment.sh
@@ -2,15 +2,14 @@ printf '\n\e[1m%-10s \t %-10s \t %-10s \t \e[0m\n' NAME VERSION STATUS
 
 while read item; do
 
-    regex="([0-9]+).([0-9]+).([0-9]+)"
+    version=($(${item} --version "exit" 2> /dev/null | egrep -o "([0-9]+).([0-9]+).([0-9]+)"))
 
-    version=($(${item} --version "exit" 2> /dev/null | egrep -o ${regex}))
-
-    if [[ $version =~ $regex ]]; then
-        printf '%-10s \t %-10s \t %-1s \e[32m\xE2\x9C\x94\e[39m \n' $item $version ok
+    if [[ $version == "" ]]; then
+        printf '%-10s \t %-10s \t %-1s \e[31m\xE2\x9D\x8C\e[39m \n' $item - not-found
     else
-        printf '%-10s \t %-10s \t %-1s \xE2\x9D\x8C \n' $item - not-found
+        printf '%-10s \t %-10s \t %-1s \e[32m\xE2\x9C\x94\e[39m \n' $item $version ok
     fi
+
 done < development-environment
 
 echo -e "\e[39m\e[0m"
